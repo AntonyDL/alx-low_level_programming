21ece0bb606abbb7330089fe8fc82c3e7cd35ef4
AntonyDL <anyonymandla@gmail.com>
2023-02-18 00:13:05 +0200

    add:0-positive_or_negative.c fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,11 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
+/* main - a program that prints whether the number stored
+ * in the variable n is positive or negative
+ *
+ * Return: program always return's a 0 as success
+ */
 
 int main(void)
 	{
@@ -12,21 +17,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n>0)
-	{
-		printf ("is positive");
-	}
+		printf ("%dis positive");
+	else if (n==0)
+		printf ("%dis zero");
 	else
-	{ 
-		if (n=0)
-		{
-			printf ("is zero");
-		}
-		else
-			if (n<0)
-			{
-				printf ("is negative\n");
-	
-			}		
+		printf ("%dis negative\n", n);		
 	}
 	return (0);
 }

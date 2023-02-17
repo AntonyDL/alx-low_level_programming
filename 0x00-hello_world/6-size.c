8a7c294bd5f2426560d71d876684fe06ecf37759
AntonyDL <anyonymandla@gmail.com>
2023-02-17 21:53:11 +0200

    add:6-size.c fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-	printf("size of char: %lu byte(s)\n", (unsigned long) sizeof(a));
-	printf("size of int: %lu byte(s)\n", (unsigned long) sizeof(b));
-	printf("size of long int: %lu byte(s)\n", (unsigned long) sizeof(c));
-	printf("size of long long int: %lu byte(s)\n", (unsigned long) sizeof(d));
-	printf("size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
+	printf("size of char: %lu byte(s)\n", sizeof(char));
+	printf("size of int: %lu byte(s)\n", sizeof(int));
+	printf("size of long int: %lu byte(s)\n", sizeof(int));
+	printf("size of long long int: %lu byte(s)\n", sizeof(int));
+	printf("size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }

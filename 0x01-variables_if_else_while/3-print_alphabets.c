6f0d5c92e06e4b85f1052d090cd825156101b722
AntonyDL <anyonymandla@gmail.com>
2023-02-19 21:25:01 +0200

    add: modified 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,14 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	int i;
+	{ 
+		int i;
 
-	for (i = 97; i < 123; i++)
-		putchar(i);
-	for (i = 65; i < 91; i++)
-		putchar(i);
+		for (i = 97; i < 123; i++)
+			putchar(i);
+		for (i = 65; i < 91; i++)
+			putchar(i);
 
 	putchar('\n');
 	return (0);
-}
+	}	

4600d8fdd7f1252e776c176d5c6f57367fd41613
AntonyDL <anyonymandla@gmail.com>
2023-02-23 12:30:22 +0200

    add:modified 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
-
 /**
  * main- Entry Point
  *
- * print_alphabet: a program that prints all alphabets 
+ * print_alphabet: a program that prints all alphabets
  * in lowercasefollowed by a new line
  *
  */
-
 void print_alphabet(void)
 {
 	char letter;

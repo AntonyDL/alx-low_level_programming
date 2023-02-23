0cf4c2b9e2f1539335a91c4842526911359b4155
AntonyDL <anyonymandla@gmail.com>
2023-02-23 13:38:19 +0200

    add: 8-24_hours.c & 8-main.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ * starting from 00:00 to 23:59
+ *
+ * Return: 0 as (Success)
+ */
+void jack_bauer(void)
+{
+	int i, a;
+
+	i=0;
+
+	while (1<60)
+	{
+		a=0;
+		while (a<60)
+		{
+			_putchar((i/10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar('\n);
+			a++;
+		}
+	}	i++
+
+}

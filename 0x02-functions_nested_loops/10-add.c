cf12072b900e7a12607cf606fb818fd864057e62
AntonyDL <anyonymandla@gmail.com>
2023-02-23 14:39:44 +0200

    add: 10-add.c,10-main.c,11-main.c,11-print_to_98.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * int_add - a function that adds two integers,
+ * and returns the result
+ *
+ * Return: Always return 0.
+ */
+
+int add(int, int)
+{
+	int a;
+	int b;
+	int res;
+
+	res = a + b;
+	printf("The results of a+b is :%d\n", res);
+	return(0);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_to_98 - function that prints all natural number
+ * form n to 98, followed by a new line
+ *
+ * Return: Always a (0) as (Success)
+ */
+
+void print_to_98(int n)
+{
+	if (n=0 && n<98, i++)
+	{
+		_putchar("%n\n", n);
+		i++;
+
+		_putchar(',');
+		_puthcar(' ');
+		_putchar('a-z', '0-98');
+		_putchar(first_number=0);
+		_putchar(last_number=98);
+
+		_putchar("\n");
+		return (0);
+	}
+}

5084167f8c1377cfc41234075f105d4bbc2abc8b
AntonyDL <anyonymandla@gmail.com>
2023-02-23 21:29:05 +0200

    add:4-print_alpha.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,12 +13,11 @@ int main(void)
 	char letter = 'a';
 
 	while (letter <= 'z')
-		{
+	{
 		if (letter != 'e' && letter != 'q')
 			putchar(letter);
 			letter ++;
-		}
-
+	}
 	putchar('\n');
 
 	return (0);

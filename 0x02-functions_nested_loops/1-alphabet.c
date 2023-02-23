ec9cd17c15f6810b37b699a45bb2b91611534e35
AntonyDL <anyonymandla@gmail.com>
2023-02-23 12:42:40 +0200

    add:modified 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,10 +2,12 @@
 
 /**
  * main- Entry Point
- * Desription: print_alphabet
- * print_alphabet: a program that prints all alphabets
- *in lowercasefollowed by a new line
  *
+ *Desription: print_alphabet
+ *print_alphabet: a program that prints all alphabets
+ *in lowercasefollowed by a new line
+
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
